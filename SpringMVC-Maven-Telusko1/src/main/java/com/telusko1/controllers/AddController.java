7f73dbfb7f895941da8b4d29c563a8f3609db188
Wahid1 <Wahid1@DESKTOP-DE4OEA8>
2020-05-28 19:46:59 -0700

    Added the Service Class and Service methods to process the logic:
    Instead of processing the data / logic inside the controllers we can
    make Service class to handle the processing and we can simply call our
    services inside the controllers

--- SpringMVC-Maven-Telusko1/src/main/java/com/telusko1/controllers/AddController.java
+++ SpringMVC-Maven-Telusko1/src/main/java/com/telusko1/controllers/AddController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.telusko1.services.AddService;
+
 @Controller
 public class AddController {
 	@RequestMapping("/add")
@@ -14,7 +16,10 @@ public class AddController {
 	{
 		int num1 = Integer.parseInt(request.getParameter("n1"));
 		int num2 = Integer.parseInt(request.getParameter("n2"));
-		int result = num1+num2;
+		
+		//creating object of Service class calling its method to do the processing for us.
+		AddService addService = new AddService();
+		int result = addService.add(num1, num2);
 		
 		
 		//to pass the data to view we make use of ModelAndView Object
---
+++ SpringMVC-Maven-Telusko1/src/main/java/com/telusko1/services/AddService.java
@@ -0,0 +1,9 @@
+package com.telusko1.services;
+
+public class AddService {
+	
+	public int add(int num1, int num2)
+	{
+		return num1+num2;
+	}
+}
